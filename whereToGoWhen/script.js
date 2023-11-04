ef7a62739969815621cea868a3666ecfa44edfe9
bme3412 <54747855+bme3412@users.noreply.github.com>
2023-11-03 22:58:47 -0400

    Add files via upload

--- whereToGoWhen/script.js
+++ whereToGoWhen/script.js
@@ -1,33 +1,49 @@
-document.getElementById('nextPageButton').addEventListener('click', function() {
-    // Fetch the content of ideas.html
-    fetch('ideas.html')
+document.addEventListener('DOMContentLoaded', function() {
+  loadIdeasContent();
+  attachFilterButtonEvent();
+});
+
+function loadIdeasContent() {
+  fetch('ideas.html')
     .then(response => response.text())
     .then(content => {
-        // Insert the content into the ideas-content div
-        document.querySelector('.ideas-content').innerHTML = content;
-        
-        // Smoothly scroll to the ideas-content div
-        document.querySelector('.ideas-content').scrollIntoView({ behavior: 'smooth' });
-        
-        // Populate the date after adding ideas.html to index.html
-        populateDate();
-
-        // Add an event listener to the month selection dropdown to update the default date when the dropdown changes
-        document.getElementById('monthSelection').addEventListener('change', populateDate);
+      document.querySelector('.ideas-content').innerHTML = content;
+      populateDate();
+      attachIdeaEventListeners();
+      fetchDataAndDisplay();
     })
     .catch(error => {
-        console.error('There was an error fetching the content:', error);
+      console.error('There was an error fetching the content:', error);
     });
-});
+}
+
+function attachFilterButtonEvent() {
+  const filterButton = document.getElementById('filterButton');
+  const filtersContainer = document.querySelector('.filters-container');
+
+  filterButton.addEventListener('click', function() {
+    filtersContainer.classList.toggle('active');
+  });
+
+  // Hide filters when clicking outside of the filters container
+  window.addEventListener('click', function(event) {
+    if (!filterButton.contains(event.target) && filtersContainer.classList.contains('active')) {
+      filtersContainer.classList.remove('active');
+    }
+  });
+}
 
 function populateDate() {
     // Get the current date
     const today = new Date();
     const dd = String(today.getDate()).padStart(2, '0');
     const mm = String(today.getMonth() + 1).padStart(2, '0'); // January is 0!
+    const yyyy = today.getFullYear();
+    const monthNames = ["January", "February", "March", "April", "May", "June",
+                        "July", "August", "September", "October", "November", "December"];
 
     // Format the current date in the desired format: e.g., "October 28, 2023"
-    const currentFormattedDate = `${mm}/${dd}/${today.getFullYear()}`;
+    const currentFormattedDate = `${monthNames[today.getMonth()]} ${dd}, ${yyyy}`;
 
     // Set the date for the "Today is" section
     const currentDateElement = document.getElementById('currentDate');
@@ -36,15 +52,22 @@ function populateDate() {
     }
 
     // Get the number of months selected from the dropdown
-    const monthsToAdd = parseInt(document.getElementById('monthSelection').value);
+    const monthsToAdd = parseInt(document.getElementById('monthSelection').value, 10);
 
-    // Calculate the date "monthsToAdd" months from now
-    today.setMonth(today.getMonth() + monthsToAdd);
-    const futureDD = String(today.getDate()).padStart(2, '0');
-    const futureMM = String(today.getMonth() + 1).padStart(2, '0'); 
+    // Calculate the date "monthsToAdd" months from now, adjusting for the end of the month.
+    const futureDate = new Date(yyyy, today.getMonth() + monthsToAdd, dd);
+    // Check if the month rolled over unexpectedly due to adding months (e.g., Jan 31 + 1 month = Mar 03)
+    if (futureDate.getDate() !== today.getDate()) {
+        // Adjust to the last day of the previous month
+        futureDate.setDate(0);
+    }
+
+    const futureDD = String(futureDate.getDate()).padStart(2, '0');
+    const futureMM = String(futureDate.getMonth() + 1).padStart(2, '0'); 
+    const futureYYYY = futureDate.getFullYear();
 
     // Format the future date in the format required for the date input
-    const futureFormattedDate = `${today.getFullYear()}-${futureMM.padStart(2, '0')}-${futureDD}`;
+    const futureFormattedDate = `${futureYYYY}-${futureMM}-${futureDD}`;
 
     // Set the default date for the search container input
     const startDateElement = document.querySelector('input[aria-label="Start Date"]');
@@ -53,6 +76,214 @@ function populateDate() {
     }
 }
 
-// Add an event listener to the month selection dropdown to update the default date when the dropdown changes
-document.getElementById('monthSelection').addEventListener('change', populateDate);
+function attachIdeaEventListeners() {
+  const filterButton = document.getElementById('filterButton');
+  const filtersContainer = document.querySelector('.filters-container');
 
+  // Initially hide filter content
+  function hideFilterContent() {
+    filtersContainer.style.display = 'none';
+  }
+
+  // Toggle filter content visibility
+  function toggleFilterContent() {
+    const isHidden = filtersContainer.style.display === 'none';
+    filtersContainer.style.display = isHidden ? 'block' : 'none';
+  }
+
+  // Event listener for the filter button
+  if (filterButton) {
+    filterButton.addEventListener('click', (event) => {
+      event.stopPropagation();
+      toggleFilterContent();
+    });
+  }
+
+  // Hide the filter content when clicking outside
+  function handleCloseFilters(event) {
+    if (!filtersContainer.contains(event.target)) {
+      hideFilterContent();
+    }
+  }
+
+  // Call hideFilterContent on load to ensure filter contents are hidden
+  hideFilterContent();
+
+  document.body.addEventListener('click', handleCloseFilters);
+}
+
+
+document.getElementById('filterButton').addEventListener('click', function() {
+  document.querySelector('.filter-content').classList.toggle('active');
+  document.querySelector('.main-content').classList.toggle('active');
+});
+
+
+function fetchDataAndDisplay(region) {
+    // Fetch data from the server
+    fetch('http://localhost:3000/data')
+        .then(response => response.json())
+        .then(data => {
+            // Filter data for the selected region
+            let filteredData;
+            if (region === 'Surprise Me!') {
+                // Implement logic for surprise me or simply select a random region
+                const regions = ['Asia', 'Europe', 'Americas', 'Latin America', 'Africa', 'Pacific'];
+                const randomRegion = regions[Math.floor(Math.random() * regions.length)];
+                filteredData = data.filter(row => row.Continent === randomRegion);
+            } else {
+                filteredData = data.filter(row => row.Continent === region);
+            }
+            displayDataInGrid(filteredData);
+        })
+        .catch(error => {
+            console.error('Error fetching data:', error);
+        });
+}
+
+/**
+ * Fetches data and updates the display based on the selected region.
+ * @param {string} region - The region for which to fetch and display data.
+ */
+function fetchDataAndDisplay(region) {
+    const dataEndpoint = 'http://localhost:3000/data';
+    const errorMessage = 'Error occurred while fetching data:';
+  
+    fetch(dataEndpoint)
+      .then(response => response.json())
+      .then(data => {
+        const filteredData = filterDataByRegion(data, region);
+        updateGridDisplay(filteredData);
+      })
+      .catch(error => console.error(`${errorMessage} ${error}`));
+  }
+  
+  /**
+   * Filters data by region or selects a random region if 'Surprise Me!' is chosen.
+   * @param {Array} data - The data to be filtered.
+   * @param {string} region - The selected region to filter by.
+   * @returns {Array} - The filtered data.
+   */
+  function filterDataByRegion(data, region) {
+    if (region === 'Surprise Me!') {
+      const regions = ['Asia', 'Europe', 'Americas', 'Latin America', 'Africa', 'Pacific'];
+      const randomRegion = regions[Math.floor(Math.random() * regions.length)];
+      return data.filter(item => item.Continent === randomRegion);
+    }
+    return data.filter(item => item.Continent === region);
+  }
+  
+  /**
+   * Updates the grid display with the provided data.
+   * @param {Array} data - The data to be displayed.
+   */
+  function updateGridDisplay(data) {
+    // Sort the data alphabetically by city name
+    const sortedData = data.sort((a, b) => a.CityServed.localeCompare(b.CityServed));
+  
+    const gridSelector = '.ideas-content .photo-grid';
+    const gridContainer = createOrReplaceGrid(gridSelector);
+  
+    const observer = initializeIntersectionObserver();
+  
+    // Use sortedData instead of data for creating grid tiles
+    sortedData.forEach(item => {
+      const gridTile = createGridTile(item, observer);
+      gridContainer.appendChild(gridTile);
+    });
+  
+    document.querySelector('.ideas-content').appendChild(gridContainer);
+  }
+
+  
+  
+  /**
+   * Creates or replaces the existing grid container.
+   * @param {string} selector - The CSS selector for the grid.
+   * @returns {HTMLElement} - The created grid container.
+   */
+  function createOrReplaceGrid(selector) {
+    let grid = document.querySelector(selector);
+    if (grid) {
+      grid.remove();
+    }
+    grid = document.createElement('div');
+    grid.className = 'photo-grid';
+    return grid;
+  }
+  
+  /**
+   * Initializes an intersection observer for lazy loading images.
+   * @returns {IntersectionObserver} - The initialized observer.
+   */
+  function initializeIntersectionObserver() {
+    const observerOptions = {
+      rootMargin: '100px',
+      threshold: 0.1
+    };
+  
+    return new IntersectionObserver((entries, observer) => {
+      entries.forEach(entry => {
+        if (entry.isIntersecting) {
+          const image = entry.target;
+          if (!image.src) { // Check if the image src is not already set
+            image.classList.add('lazy-loaded');
+            image.src = image.dataset.src;
+          }
+          observer.unobserve(image); // Stop observing the image that has been loaded
+        }
+      });
+    }, observerOptions);
+  }
+  
+  
+  /**
+   * Creates a grid tile element with lazy-loaded image.
+   * @param {Object} dataItem - An object containing the data for the grid tile.
+   * @param {IntersectionObserver} observer - The observer for lazy loading.
+   * @returns {HTMLElement} - The created grid tile element.
+   */
+  function createGridTile(dataItem, observer) {
+    const tile = document.createElement('div');
+    tile.className = 'grid-tile';
+  
+    if (dataItem.PhotoURL) {
+      const photoWrapper = document.createElement('div');
+      photoWrapper.className = 'photo-wrapper';
+  
+      const photo = document.createElement('img');
+      photo.dataset.src = dataItem.PhotoURL;
+      photo.alt = `${dataItem.CityServed} photo`;
+      photo.className = 'grid-photo lazy';
+      observer.observe(photo);
+  
+      photoWrapper.appendChild(photo);
+      photoWrapper.appendChild(createOverlay(dataItem));
+      tile.appendChild(photoWrapper);
+    }
+  
+    return tile;
+  }
+  
+  /**
+   * Creates an overlay element with country and city labels.
+   * @param {Object} dataItem - An object containing the country and city data.
+   * @returns {HTMLElement} - The created overlay element.
+   */
+  function createOverlay(dataItem) {
+    const overlay = document.createElement('div');
+    overlay.className = 'overlay';
+  
+    const countryLabel = document.createElement('div');
+    countryLabel.className = 'grid-label overlay-label';
+    countryLabel.textContent = dataItem.CityServed;
+  
+    const cityLabel = document.createElement('div');
+    cityLabel.className = 'grid-label overlay-label';
+    cityLabel.textContent = dataItem.Country;
+  
+    overlay.appendChild(countryLabel);
+    overlay.appendChild(cityLabel);
+  
+    return overlay;
+  }
---
+++ whereToGoWhen/server.js
@@ -0,0 +1,25 @@
+const express = require('express');
+const csv = require('csv-parser');
+const fs = require('fs');
+const cors = require('cors');  // Add this line
+
+const app = express();
+const PORT = 3000;
+
+app.use(cors());  // Add this line
+app.use('/assets', express.static('/Users/brendan/Desktop - Brendan’s MacBook Air/whereToGoWhen/assets'));
+
+app.get('/data', (req, res) => {
+    const results = [];
+
+    fs.createReadStream("/Users/brendan/Desktop - Brendan’s MacBook Air/whereToGoWhen/data/euro_photos.csv")
+        .pipe(csv())
+        .on('data', (data) => results.push(data))
+        .on('end', () => {
+            res.json(results);
+        });
+});
+
+app.listen(PORT, () => {
+    console.log(`Server is running at http://localhost:${PORT}`);
+});
